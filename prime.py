bedbc5a89171cb7cc02e7c4e03611a752b553650
Parthsalke <parthsalke7@gmail.com>
2023-07-24 18:27:52 +0530

    modified prime.py

--- prime.py
+++ prime.py
@@ -5,3 +5,11 @@ def is_prime(number):
         if number % i == 0:
             return False
     return True
+
+
+num = 17
+
+if is_prime(num):
+    print(f"{num} is a prime number.")
+else:
+    print(f"{num} is not a prime number.")
