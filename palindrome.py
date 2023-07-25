30265c2342da748866e1b8af435905fc0507288c
Parthsalke <parthsalke7@gmail.com>
2023-07-25 10:50:16 +0530

    palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+def is_palindrome(number):
+    # Convert the number to a string for easy comparison
+    num_str = str(number)
+    # Compare the original number with its reverse
+    return num_str == num_str[::-1]
+num = 121
+
+if is_palindrome(num):
+    print(f"{num} is a palindrome number.")
+else:
+    print(f"{num} is not a palindrome number.")
